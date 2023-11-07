0e8c9e52af1e832e35683930b23d62b090875ef8
Yael Brodsky <brodskyy88@gmail.com>
2023-11-07 18:06:08 -0300

    fix migration

--- postgrator/migrations/013.do.change-users-permissions-fields.sql
+++ postgrator/migrations/013.do.change-users-permissions-fields.sql
@@ -1,14 +1,1 @@
-UPDATE `users` SET `permissions` = JSON_OBJECT(
-        "routesList", true,
-        "routesMap", true,
-        "routesCreateForDriver", true,
-        "routesCreateDeliveryOrder", false,
-        "ordersList", true,
-        "ordersListSupplier", true,
-        "ordersCreate", true,
-        "customersCreate", true,
-        "customersHideLocationRelatedFields", false,
-        "toursCreate", true,
-        "showMasterData", true,
-        "showTourTab:, true
-);
+UPDATE `users` SET `permissions` = JSON_INSERT(`permissions`, '$.showTourTab', true);
